56a2d7f149e67b00eeae7bada7500d00081f8141
Deicy Hernandez Zamora <licdeicyhernandez@gmail.com>
2026-07-22 18:08:26 +0200

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,8 @@
+URL_SERVICE = "https://cnt-19aefd71-75f5-48c9-b7aa-2be728b27f97.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+
+
